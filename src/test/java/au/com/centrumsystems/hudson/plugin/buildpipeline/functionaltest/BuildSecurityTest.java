4a90e9fd2b09144c8f1c6c3d23a4b5e524400948
tomakehurst <t.m.akehurst@googlemail.com>
2013-10-17 10:51:15 +0100

    Refactored tests - moved webdriver based test into a dedicated package and pulled out a parent class.

--- src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/functionaltest/BuildSecurityTest.java
+++ src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/functionaltest/BuildSecurityTest.java
@@ -1,55 +1,32 @@
-package au.com.centrumsystems.hudson.plugin.buildpipeline;
+package au.com.centrumsystems.hudson.plugin.buildpipeline.functionaltest;
 
 import au.com.centrumsystems.hudson.plugin.buildpipeline.testsupport.BuildCardComponent;
-import au.com.centrumsystems.hudson.plugin.buildpipeline.testsupport.LoginLogoutPage;
-import au.com.centrumsystems.hudson.plugin.buildpipeline.testsupport.PipelinePage;
+import au.com.centrumsystems.hudson.plugin.buildpipeline.testsupport.PipelineWebDriverTestBase;
 import au.com.centrumsystems.hudson.plugin.buildpipeline.trigger.BuildPipelineTrigger;
-import com.google.common.base.Function;
 import hudson.model.FreeStyleProject;
 import hudson.model.Item;
 import hudson.plugins.parameterizedtrigger.AbstractBuildParameters;
 import hudson.security.GlobalMatrixAuthorizationStrategy;
 import hudson.security.Permission;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.FailureBuilder;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.FluentWait;
 
 import java.util.Collections;
-import java.util.concurrent.TimeUnit;
 
-import static hudson.model.Result.SUCCESS;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class BuildSecurityTest {
+public class BuildSecurityTest extends PipelineWebDriverTestBase {
 
-    @Rule
-    public JenkinsRule jr = new JenkinsRule();
-
-    static final String INITIAL_JOB = "secured-job";
     static final String SECOND_JOB = "second-job";
     static final String UNPRIVILEGED_USER = "unprivilegeduser";
     static final String PRIVILEGED_USER = "privilegeduser";
 
-    JenkinsRule.DummySecurityRealm realm;
-    FreeStyleProject initialJob;
     FreeStyleProject secondJob;
-
-    BuildPipelineView pipelineView;
-    LoginLogoutPage loginLogoutPage;
-    PipelinePage pipelinePage;
-    private WebDriver webDriver;
 
     @Before
     public void init() throws Exception {
-        realm = jr.createDummySecurityRealm();
-        jr.jenkins.setSecurityRealm(realm);
         GlobalMatrixAuthorizationStrategy authorizationStrategy = new GlobalMatrixAuthorizationStrategy();
         jr.jenkins.setAuthorizationStrategy(authorizationStrategy);
         authorizationStrategy.add(Permission.READ, UNPRIVILEGED_USER);
@@ -57,25 +34,10 @@ public class BuildSecurityTest {
         authorizationStrategy.add(Item.BUILD, PRIVILEGED_USER);
         authorizationStrategy.add(Item.CONFIGURE, PRIVILEGED_USER);
 
-        initialJob = jr.createFreeStyleProject(INITIAL_JOB);
-
         secondJob = jr.createFreeStyleProject(SECOND_JOB);
         secondJob.getBuildersList().add(new FailureBuilder());
         initialJob.getPublishersList().add(new BuildPipelineTrigger(SECOND_JOB, Collections.<AbstractBuildParameters>emptyList()));
         jr.jenkins.rebuildDependencyGraph();
-
-        pipelineView = new BuildPipelineView("pipeline", "Pipeline", new DownstreamProjectGridBuilder(INITIAL_JOB), "5", false, true, false, false, false, 1, null);
-        jr.jenkins.addView(pipelineView);
-
-        webDriver = new FirefoxDriver();
-        loginLogoutPage = new LoginLogoutPage(webDriver, jr.getURL());
-        pipelinePage = new PipelinePage(webDriver, pipelineView.getViewName(), jr.getURL());
-    }
-
-    @After
-    public void cleanUp() {
-        webDriver.close();
-        webDriver.quit();
     }
 
     @Test
